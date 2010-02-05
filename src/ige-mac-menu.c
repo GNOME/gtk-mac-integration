6c63d31e60c3815f41395ca63f46f9129bc35204
John Ralls <jralls@ceridwen.us>
2010-02-05 14:28:41 -0800

    Change license notice to allow distribution under later revisions of LGPL.

--- src/ige-mac-menu.c
+++ src/ige-mac-menu.c
@@ -6,10 +6,10 @@
  * For further information, see:
  * http://developer.imendio.com/projects/gtk-macosx/menubar
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; version 2.1
- * of the License.
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
