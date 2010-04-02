2a64ff9ef733192f798510cd08826aceec3f1025
John Ralls <jralls@ceridwen.us>
2010-04-02 12:34:33 -0700

    Call g_cclosure_new_object_swap instead of g_cclosure_new
    This way the closure is freed if menu_item is, and the callback function is invoked with closure.data instead of the (empty) passed-in pointer.

--- src/cocoa_menu_item.c
+++ src/cocoa_menu_item.c
@@ -425,8 +425,8 @@ cocoa_menu_item_add_item (NSMenu* cocoa_menu, GtkWidget* menu_item, int index)
     }
 
     const gchar* label_text = get_menu_label_text (menu_item, &label);
-    GClosure *menu_action = g_cclosure_new(G_CALLBACK(gtk_menu_item_activate), 
-					   menu_item, NULL);
+    GClosure *menu_action = g_cclosure_new_object_swap(G_CALLBACK(gtk_menu_item_activate), 
+						       G_OBJECT(menu_item));
     g_closure_set_marshal(menu_action, g_cclosure_marshal_VOID__VOID);
 		
     if (label_text)
