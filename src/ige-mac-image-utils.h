4269ce491ddfc47b5b3e7399f7dea8b3d6ffbfd5
John Ralls <jralls@ceridwen.us>
2010-04-04 13:13:53 -0700

    Correct license from GPL to LGPL.

--- src/ige-mac-image-utils.h
+++ src/ige-mac-image-utils.h
@@ -2,7 +2,7 @@
  * Copyright (C) 2007 Imendio AB
  *
  * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
  *
