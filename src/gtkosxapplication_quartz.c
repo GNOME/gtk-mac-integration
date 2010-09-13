4e4cbe31d4413fbcf41cf0ea92d0432d504253d7
John Ralls <jralls@ceridwen.us>
2010-09-13 13:10:03 -0700

    Cast to GNSMenuItem so that the setHidden method is found when building for Tiger. https://sourceforge.net/apps/trac/gtk-osx/ticket/41

--- src/gtkosxapplication_quartz.c
+++ src/gtkosxapplication_quartz.c
@@ -813,7 +813,7 @@ gtk_osxapplication_insert_app_menu_item (GtkOSXApplication* self,
 					 gint index) {
     cocoa_menu_item_add_item ([[[NSApp mainMenu] itemAtIndex: 0] submenu],
 			      item, index);
-    [[[[[NSApp mainMenu] itemAtIndex: 0] submenu] 
+    [(GNSMenuItem*)[[[[NSApp mainMenu] itemAtIndex: 0] submenu] 
       itemAtIndex: index] setHidden: NO];
 }
 
