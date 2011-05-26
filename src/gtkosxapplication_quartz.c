5184d8b9378814588f6e2138977c2c7c2197f50b
John Ralls <jralls@ceridwen.us>
2011-05-26 16:37:46 -0700

    Remove the attempt to add the window title to the window menu.
    First off, it's unnecessary: AppKit takes care of it. Second, it doesn't work because there's no GDK window until the window is realized, and most applications will call this during menu setup, before that happens.

--- src/gtkosxapplication_quartz.c
+++ src/gtkosxapplication_quartz.c
@@ -862,19 +862,6 @@ gtk_osxapplication_set_window_menu(GtkOSXApplication *self,
      g_return_if_fail(cocoa_item != NULL);
     [cocoa_menubar setWindowsMenu: cocoa_item];
     [NSApp setWindowsMenu: [cocoa_item submenu]];
-    parent = gtk_widget_get_toplevel(GTK_WIDGET(menubar));
-    if (parent && GTK_IS_WIDGET(parent))
-      win = gtk_widget_get_window(parent);
-    else
-      g_print("No Parent\n");
-    if (win && GDK_IS_WINDOW(win))
-      nswin = gdk_quartz_window_get_nswindow(win);
-    else
-      g_print("No GDK Window\n");
-    if (nswin)
-      [NSApp addWindowsItem: nswin title: [nswin title] filename: NO];
-    else
-      g_print ("No NSWindow\n");
  }
   else {
     GNSMenuItem *cocoa_item = create_window_menu (self);
