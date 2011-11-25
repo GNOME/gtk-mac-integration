6788f820a098e894b2c000ef9b7dc85d2de7dd29
John Ralls <jralls@ceridwen.us>
2011-11-25 15:17:11 -0800

    [Introspection] Change gtk_osxapplication to gtkosx_application in python bindings

--- bindings/python/gtkosx_application/gtkosx_application-module.c
+++ bindings/python/gtkosx_application/gtkosx_application-module.c
@@ -1,25 +1,25 @@
 #include <pygobject.h>
  
-void gtk_osxapplication_register_classes (PyObject *d); 
-void pygtk_osxapplication_add_constants(PyObject *module, const gchar *strip_prefix);
+void gtkosx_application_register_classes (PyObject *d); 
+void pygtkosx_application_add_constants(PyObject *module, const gchar *strip_prefix);
 
-extern PyMethodDef gtk_osxapplication_functions[];
+extern PyMethodDef gtkosx_application_functions[];
  
 DL_EXPORT(void)
-init_gtk_osxapplication (void)
+init_gtkosx_application (void)
 {
   PyObject *m, *d;
 
   init_pygobject ();
 
-  m = Py_InitModule ("gtk_osxapplication._gtk_osxapplication", 
-                     gtk_osxapplication_functions);
+  m = Py_InitModule ("gtkosx_application._gtkosx_application", 
+                     gtkosx_application_functions);
   d = PyModule_GetDict (m);
 
-  gtk_osxapplication_register_classes (d);
+  gtkosx_application_register_classes (d);
 
   if (PyErr_Occurred ()) { 
     PyErr_Print();
-    Py_FatalError ("can't initialize module gtk_osxapplication:");
+    Py_FatalError ("can't initialize module gtkosx_application:");
   }
 }
