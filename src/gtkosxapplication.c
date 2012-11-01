021cd056466e3e4a1d99e161975dcb5a1f77c3b5
John Ralls <jralls@ceridwen.us>
2012-11-01 14:05:35 -0700

    Add documentation/introspection annotation for gtkosx_application_get()

--- src/gtkosxapplication.c
+++ src/gtkosxapplication.c
@@ -230,6 +230,11 @@
  * from quitting.
  */
 
+/**
+ * gtkosx_application_get:
+ *
+ * Returns: (transfer none) the singleton application object.
+ */
 GtkosxApplication *
 gtkosx_application_get (void)
 {
