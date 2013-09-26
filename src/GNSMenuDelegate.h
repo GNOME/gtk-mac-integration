37a852198290d96a54333781e235dccf7db8d24b
John Ralls <jralls@ceridwen.us>
2013-09-26 13:44:46 -0700

    Prevent "doesn't implement NSApplicationDelegate" error
    When building with OSX 10.6 and later.

--- src/GNSMenuDelegate.h
+++ src/GNSMenuDelegate.h
@@ -1,5 +1,9 @@
 #import <Cocoa/Cocoa.h>
 #include <gtk/gtk.h>
 
+#if (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6)
+@interface _GNSMenuDelegate : NSObject <NSMenuDelegate> {}
+#else
 @interface _GNSMenuDelegate : NSObject {}
+#endif
 @end
