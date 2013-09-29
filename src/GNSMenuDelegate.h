2240257c7f7cb075b43e844bae0749bd59005c80
John Ralls <jralls@ceridwen.us>
2013-09-29 16:54:08 -0700

    Fix minimum version to one that exists in 10.5

--- src/GNSMenuDelegate.h
+++ src/GNSMenuDelegate.h
@@ -1,7 +1,7 @@
 #import <Cocoa/Cocoa.h>
 #include <gtk/gtk.h>
 
-#if (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6)
+#if (MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_5)
 @interface _GNSMenuDelegate : NSObject <NSMenuDelegate> {}
 #else
 @interface _GNSMenuDelegate : NSObject {}
